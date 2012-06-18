2f1b75bc5d0d00893b7162e9658e30395db88414
Joe Khoobyar <joe@ankhcraft.com>
2012-06-18 17:40:49 -0400

    added simple helper class for basic use cases

--- src/main/java/name/khoobyar/joe/jsch/JSchSSPI.java
+++ src/main/java/name/khoobyar/joe/jsch/JSchSSPI.java
@@ -5,17 +5,47 @@ package name.khoobyar.joe.jsch.sspi;
 
 import com.jcraft.jsch.JSch;
 
-/**Simplifies usage of JSch with SSPI support on Windows XP or above.
+/**	<h3>Simplifies usage of JSch with native Kerberos support on Windows XP or above.
+ *	</h3>
  *
+ *	<h6>
+ *	Target audience
+ *	</h6>
+ *
+ *  <p>Developers who want/need transparent support for single sign-on from Windows clients
+ *  to SSH servers (Windows or Linux).  Assumes that your Kerberos provider on Windows integrates
+ *	with Microsoft's SSPI (as a security provider implementation) - since this is why you would be interested in the first place.
+ *
+ *	<h6>
+ *	Background Information <small>(why this library is useful at all)</small>
+ *	</h6>
+ *	<p>
+ *  Microsoft Windows provides it's own <em>Kerberos vendor-neutral</em> API called SSPI
+ *	that is semantically <em>very</em> similar to the GSS API supported by most other major 
+ *	operating systems.  Recent Java releases from Oracle, however, <em>still</em> do not provide
+ *	support for integrating with this non-standard GSS alternative this Microsoft, even after working patches have been provided by the community over at Open JDK for about four years now.
+ *	</p>
+ *	<p>
+ *	Vendors providing single sign-on on Windows intranets generally don't provide support for all things Java Security.  However, this support can be quite effective at increasing IT staff productivity within a corporate LAN environment - especially if those intranets support cross-forest authentication a mixed (Windows/Linux) environment).
+ *	</p>
  *
  * @author Joe Khoobyar
  */
-public abstract class JSchSSPI {
+public class JSchSSPI
+	extends JSch
+{
 
-	/** Conditionally configure the given JSch instance
-	 *  for SSPI support if the host OS is Windows XP or above.
-	 *
-	 *  Otherwise, the configuration of the given JSch instance will be left as is.
+	/** 
+	 *	Simplest way to <em>create</em> a JSch instance that supports native Kerberos
+	 *	implementations on Windows XP or above.
+	 */
+	public JSchSSPI () {
+		super ();
+		configure (this);
+	}
+
+	/** Simplest way to <em>configure</em> an existing JSch that supports native Kerberos
+	 *	implementations on Windows XP or above.
 	 */
 	public static JSch configure (JSch jsch) {
 		String osname = System.getProperty ("os.name");
